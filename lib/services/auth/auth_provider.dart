bfa0b09610300c7e8911d40f4575e9dbcca94499
Jan Osusky <codes.osusky@outlook.com>
2024-04-07 18:21:51 +0200

    step-10

--- lib/services/auth/auth_provider.dart
+++ lib/services/auth/auth_provider.dart
@@ -5,7 +5,7 @@ import 'package:mynotes/services/auth/auth_user.dart';
 abstract class AuthProvider {
   Future<void> initialize();
   AuthUser? get currentUser;
-  Future<AuthUser?> logIn({
+  Future<AuthUser> logIn({
     required String email,
     required String password,
   });
